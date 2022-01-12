d88aa2e06f2ee320f7a18cdb583d90482abda0c0
Trey R <84750467+trusell09@users.noreply.github.com>
2022-01-12 17:54:55 -0600

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+print ("Hello World")
+
+name = "Trey"
+print("Hello",  name)
+print("Hello" + name)
+
+name = 42
+print("Hello", name)
+
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print(f"I love to eat {fave_food1} and {fave_food2}")
