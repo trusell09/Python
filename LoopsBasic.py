0f76e848177b52437aa8cf89da5e422036e01fe0
Trey R <84750467+trusell09@users.noreply.github.com>
2022-01-12 20:55:39 -0600

    Add files via upload

---
+++ LoopsBasic.py
@@ -0,0 +1,52 @@
+low = 0
+high = 151
+
+for i in range(low, high):
+    print(i)
+
+
+
+for x in range(5,1001, 5):
+    print(x)
+    
+
+for i in range(1, 101, 1):
+        if (i % 10 == 0):
+            print("Coding Dojo")
+        elif (i % 5 == 0):
+            print("Coding")
+        else:
+            print(i)
+
+for i in range(1, 101, 1):
+        if (i % 10 == 0):
+            print("Coding Dojo")
+        elif (i % 5 == 0):
+            print("Coding")
+        else:
+            print(i)
+
+
+int = 1
+int2 = 0
+while int < 500000:
+    if int % 2 != 0:
+        int2 = int2 + int
+    int += 1
+
+print(int2)
+
+for x in range(2018,0 , -4):
+    print(x)
+
+low = 2
+high = 9
+mult = 3
+
+for i in range (low, high+1, 1):
+    if i % mult == 0:
+        print(i)
+
+
+
+
