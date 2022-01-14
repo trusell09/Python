6491072d7284ca40edead444fec2dc7527b64ad3
Trey R <84750467+trusell09@users.noreply.github.com>
2022-01-14 13:38:25 -0600

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,47 @@
+from math import *
+
+phrase = "Yujiro Hanma"
+print(phrase.index("Y"))
+print(phrase.replace("Yujiro","Baki"))
+print(8 % 3)
+print(pow(4,2))
+print(max(18,12))
+print(round(3.3))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
+
+"""
+name = input("Enter your name: ")
+age = input("Enter your age:")
+print ("Hello " + name + "! you are " + age)
+"""
+"""
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = int(num1) + int(num2)
+print(result)
+"""
+
+"""
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
+"""
+
+
+"""
+player = input("Enter a baseball player: ")
+position = input("Enter positon player played: ")
+team = input ("Enter team player played for: ")
+print(player)
+print(player + " was a " + position)
+print(player + " played for the " + team)
+"""
+
+"""
+def sayhey(name, age):
+    print("Hey " + name + " you are " + age)
+sayhey("Jill", "82")
+"""
