baa8ca38e7d687a0d625f47e53f569c2f1e26945
Trey R <84750467+trusell09@users.noreply.github.com>
2022-01-17 22:57:02 -0600

    Add files via upload

---
+++ Templating/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect, session, request
+app = Flask(__name__)
+
+
+@app.route('/<name>')
+def index(name):
+    print(f"the name is: {name}")
+    return render_template('index.html', name=name, num=8)
+
+@app.route("/advanced")
+def advanced():
+    
+    student_info = [
+        {'name': 'Jimbo', 'age' : 37},
+        {'name': 'Mimbo', 'age' : 27},
+        {'name': 'Pimbo', 'age' : 17},
+        {'name': 'Gimbo', 'age' : 7},
+    ]
+
+
+
+    return render_template("adv.html", students = student_info)
+
+
+@app.route("/httpresponse")
+def http():
+    return "This is a Http Response"
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
