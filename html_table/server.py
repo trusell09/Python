d95f1e3f5831d9d842b3811c32360cbc02b1d5af
Trey R <84750467+trusell09@users.noreply.github.com>
2022-01-19 14:47:30 -0600

    Add files via upload

---
+++ html_table/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, session, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/people")
+def people():
+    
+    personal_info = [
+        {'first': 'First Name', 'last' : 'Last Name'},
+        {'first': 'Michael', 'last' : 'Choi'},
+        {'first': 'John', 'last' : 'Supsupin'},
+        {'first': 'Mark', 'last' : 'Guillen'},
+        {'first': 'KB', 'last' : 'Tonel'}
+    ]
+
+
+
+    return render_template("index.html", names = personal_info)
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True) 
